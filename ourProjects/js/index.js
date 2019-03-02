36ec93acf24fbae9ba4e21a1de60993ee5ec9ef2
mpascualach <miguelpacheson@hotmail.es>
2019-03-02 18:47:05 +0100

    applied onomousecover greyscale functionality to ourProjects and otherWorks

--- ourProjects/js/index.js
+++ ourProjects/js/index.js
@@ -1,7 +1,8 @@
+let selectedProject;
+let extendedProject;
+
 function selectProject(id){
-    let itemId = id.substring(4,5);
-    if (itemId % 2 == 0) {
-        sum = itemId / 2;
-        console.log(sum);
-    }
+    if (selectedProject) $("#"+id).removeClass("selected");
+    $("#"+id).addClass("selected");
+    selectedProject = id.substring(4,id.length);
 }
