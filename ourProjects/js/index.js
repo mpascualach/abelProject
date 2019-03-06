2c4f923a58919092b8d9b6abd03f02033c8d0147
mpascualach <miguelpacheson@hotmail.es>
2019-03-06 19:26:26 +0100

    minor fixes to loveDeath

--- ourProjects/js/index.js
+++ ourProjects/js/index.js
@@ -4,20 +4,20 @@ function selectProject(id){
     let itemId = id.substring(4,id.length);
     if (oldSelectedProject - itemId == -1 || oldSelectedProject - itemId == 1){
         if (oldSelectedProject == 1){
-            $(".momochi").css("width", "40%");
-            $(".flymmics").css("width", "60%");
+            // $(".momochi").css("width", "40%");
+            // $(".flymmics").css("width", "60%");
         }
         if (oldSelectedProject == 2){
-            $(".momochi").css("width", "60%");
-            $(".flymmics").css("width", "40%");
+            // $(".momochi").css("width", "60%");
+            // $(".flymmics").css("width", "40%");
         }
         if (oldSelectedProject == 3){
-            $(".whoops").css("width", "40%");
-            $(".dead-pet").css("width", "60%");
+            // $(".whoops").css("width", "40%");
+            // $(".dead-pet").css("width", "60%");
         }
         if (oldSelectedProject == 4){
-            $(".whoops").css("width", "60%");
-            $(".dead-pet").css("width", "40%");
+            // $(".whoops").css("width", "60%");
+            // $(".dead-pet").css("width", "40%");
         }
     }
     $(".super-selected").removeClass("super-selected");
